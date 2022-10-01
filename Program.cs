6eb3a271744aa08f18493bd5d6fc1fd1faf84a8d
sibergold <kaynakca21@hotmail.com>
2022-10-01 23:40:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace karakter_degistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cümleni gir.");
+            string[] kelimeler = Console.ReadLine().Split();
+            
+            for(int i=0;i<kelimeler.Length;i++)
+            {
+                char[] harfler=kelimeler[i].ToArray();
+                char harf=harfler[0];
+                harfler[0]=harfler[harfler.Length-1];
+                harfler[harfler.Length - 1] = harf;
+                string yenicumle=string.Concat(harfler);
+                Console.Write(yenicumle+" ");
+            }
+        
+        }
+    }
+}
